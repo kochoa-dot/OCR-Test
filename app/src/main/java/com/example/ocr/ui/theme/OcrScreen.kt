c73fafb800c0e0e29ae57b0a32afee2675d6bb96
kochoa-dot <kochoa.sv@gmail.com>
2025-01-28 01:30:19 -0600

    Funcionamiento de camara para tomar valores de codigo postal y hacer busqueda

--- app/src/main/java/com/example/ocr/ui/theme/OcrScreen.kt
+++ app/src/main/java/com/example/ocr/ui/theme/OcrScreen.kt
@@ -1,5 +1,6 @@
 package com.example.ocr.ui
 
+import android.util.Patterns
 import androidx.annotation.OptIn
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageProxy
@@ -14,11 +15,15 @@ import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.example.ocr.ui.theme.CameraPreview
+import com.google.mlkit.vision.text.Text
+import java.util.regex.Pattern
 
 @OptIn(ExperimentalGetImage::class)
 @Composable
 fun OcrScreen(navController: NavController) {
     var recognizedText by remember { mutableStateOf("") }
+
+    var isProcessing by remember { mutableStateOf(false) }
 
     val textRecognizer = remember {
         TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
@@ -32,12 +37,20 @@ fun OcrScreen(navController: NavController) {
             textRecognizer.process(inputImage)
                 .addOnSuccessListener { visionText ->
                     recognizedText = visionText.text
+
+                    val zipCode = extractZipCode(recognizedText)
+                    if (zipCode != null){
+                        navController.navigate("resultScreen/$zipCode")
+                    }else{
+                        recognizedText = "No se detectó un código postal válido"
+                    }
                 }
                 .addOnFailureListener { e ->
                     recognizedText = "Error: ${e.message}"
                 }
                 .addOnCompleteListener {
                     imageProxy.close()
+                    isProcessing = false
                 }
         }
     }
@@ -50,4 +63,11 @@ fun OcrScreen(navController: NavController) {
                 .padding(16.dp)
         )
     }
+}
+
+
+fun extractZipCode(text: String): String? {
+    val zipCodePattern = Pattern.compile("\\b\\d{5}\\b")
+    val matcher = zipCodePattern.matcher(text)
+    return if (matcher.find()) matcher.group() else null
 }
